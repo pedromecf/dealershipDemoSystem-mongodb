6ba7d676a7a05b65a5ad6c17b55e15039a461dd4
Pedro Colombano <pedromecf@gmail.com>
2021-01-22 00:28:24 -0300

    Project created and mongodb database connected

---
+++ src/main/java/br/com/dlcars/DealershipSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.dlcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dlcars/DealershipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dlcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
